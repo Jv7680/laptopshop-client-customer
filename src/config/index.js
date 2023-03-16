9200d27e3ced06e46d795316fee945d9d115ce0e
Trung Tin <justinvu768@gmail.com~>
2023-03-16 18:09:45 +0700

    changeDomain2

--- src/config/index.js
+++ src/config/index.js
@@ -3,7 +3,7 @@ export const config = {
 
     //url: "http://banlinhkienpt.us-west-2.elasticbeanstalk.com",
     // url:"http://localhost:8080"
-    url: "https://laptopshop-server.herokuapp.com/"
+    url: "https://laptopshop-server.herokuapp.com"
   },
   paypal: {
     secretKey: 'AWKEAhHCdB3iWXlD_b8NRA0mDpE1vtDhz0-58JMoYGhsC1TqaM_HSc8ChmHLHMh30wN-RFUusPidWlCR',
