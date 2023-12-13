5a8a9d04da585d34987542ba107fa387e9899661
Trung Tin <justinvu768@gmail.com~>
2023-12-13 11:48:44 +0700

    Updated BE URL

--- src/config/index.js
+++ src/config/index.js
@@ -2,7 +2,7 @@ export const config = {
   api: {
 
     // url: "http://localhost:8080"
-    url: "https://laptopshop-server.herokuapp.com"
+    url: "https://laptopshop-suda.onrender.com"
   },
   paypal: {
     secretKey: 'AWKEAhHCdB3iWXlD_b8NRA0mDpE1vtDhz0-58JMoYGhsC1TqaM_HSc8ChmHLHMh30wN-RFUusPidWlCR',
