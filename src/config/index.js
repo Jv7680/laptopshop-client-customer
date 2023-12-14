9a11b16011146ec2aed51afefe8a6dc55950a601
Trung Tin <justinvu768@gmail.com~>
2023-12-14 11:32:47 +0700

    change BE URL to railway

--- src/config/index.js
+++ src/config/index.js
@@ -2,7 +2,8 @@ export const config = {
   api: {
 
     // url: "http://localhost:8080"
-    url: "https://laptopshop-suda.onrender.com"
+    // url: "https://laptopshop-suda.onrender.com"
+    url: "https://laptopshop-server-production.up.railway.app"
   },
   paypal: {
     secretKey: 'AWKEAhHCdB3iWXlD_b8NRA0mDpE1vtDhz0-58JMoYGhsC1TqaM_HSc8ChmHLHMh30wN-RFUusPidWlCR',
